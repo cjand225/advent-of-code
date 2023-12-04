d2439ea1c58109467507583f4dd26ef0f5dbc890
James Andrews <cjand225@gmail.com>
2023-12-04 15:26:20 -0700

    Added initial solution - non-working

---
+++ problems/day-1/main.cpp
@@ -0,0 +1,54 @@
+#include <fstream>
+#include <iostream>
+
+int main()
+{
+
+    // open file
+    std::fstream inputFile("input.txt", std::fstream::in);
+
+    char first, last = '\0';
+    int result = 0;
+
+    // read file
+    char c;
+    while (inputFile.good())
+    {
+
+        // read character
+        inputFile.get(c);
+
+        // Skip characters that aren't digits or new lines
+        if (!isdigit(c) && c != '\n')
+        {
+            continue;
+        }
+
+        // if its a new line, calculate first/last and convert to a real number, reset the "numbers"
+        if (c == '\n')
+        {
+            // concat the result
+            result += (first - '0') * 10 + (last - '0');
+
+            // reset numbers
+            first = '\0';
+            last = first;
+        }
+        // if its a number, update the last digit if the first is already initialized
+        else
+        {
+            // Case where characters haven't been set or new line is encounted
+            if (first == '\0')
+            {
+                first = c;
+                last = first;
+            }
+            else
+            {
+                last = c;
+            }
+        }
+    }
+
+    std::cout << result << std::endl;
+}
