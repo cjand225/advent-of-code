7241d0cc869ab4d1bd40b0d8f1f2eab41545ad10
James Andrews <cjand225@gmail.com>
2023-12-07 14:00:27 -0700

    Added initial work for day 3 - partOne.cpp

---
+++ problems/day-3/partOne.cpp
@@ -0,0 +1,29 @@
+#include <fstream>
+#include <iostream>
+
+int main()
+{
+    // open file
+    std::fstream inputFile("input.txt", std::fstream::in);
+    std::string inputLine = "";
+
+    // Coincidentally both lines and length of lines are 140.
+    const int SIZE = 140;
+
+    // Lets store this in a 2D array of arrays.
+    char inputGrid[SIZE][SIZE];
+
+    // We can store this directly since we know the length and width of the input is the same.
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+        {
+            inputFile.get(inputGrid[i][j]);
+        }
+
+        // Ignore new lines characters.
+        inputFile.ignore();
+    }
+
+    inputFile.close();
+}
