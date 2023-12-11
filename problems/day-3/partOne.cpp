390f3e41f93b8e35b7b66f4a3ce3070b73e92a4e
James Andrews <cjand225@gmail.com>
2023-12-11 15:42:00 -0700

    Updated partOne - day 3 - nonworking solution

--- problems/day-3/partOne.cpp
+++ problems/day-3/partOne.cpp
@@ -1,13 +1,42 @@
 #include <fstream>
 #include <iostream>
 
+// Coincidentally both lines and length of lines are 140.
+const int SIZE = 140;
+
+int findAdjacency(char (&arr)[SIZE][SIZE], int x, int y)
+{
+    // If valid coordinate or a digit character
+    if (x <= (SIZE - 1) && y <= (SIZE - 1) && isdigit(arr[x][y]))
+    {
+        // go while till left most digit is found.
+        while (isdigit(arr[x][y - 1]))
+        {
+            y = y - 1;
+        }
+
+        // Start pushing to an a string.
+        std::string numberStr;
+        while (isdigit(arr[x][y]))
+        {
+            numberStr.push_back(arr[x][y]);
+            y = y + 1;
+        }
+
+        std::cout << numberStr << std::endl;
+
+        return std::stoi(numberStr);
+    }
+    else
+    {
+        return 0;
+    }
+}
+
 int main()
 {
     // open file
     std::fstream inputFile("input.txt", std::fstream::in);
-
-    // Coincidentally both lines and length of lines are 140.
-    const int SIZE = 140;
 
     // Lets store this in a 2D array of arrays.
     char inputGrid[SIZE][SIZE];
@@ -25,4 +54,44 @@ int main()
     }
 
     inputFile.close();
+
+    int partSum = 0;
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+        {
+            // Check if the current character is a symbol
+            if (!isdigit(inputGrid[i][j]) && inputGrid[i][j] != '.')
+            {
+                // Check Adjacency && find any numbers
+                // Remember edge cases where coordinates may not exist.
+
+                // Adjacency is
+                // i - 1, j - 1 -> top left
+                partSum += findAdjacency(inputGrid, i - 1, j - 1);
+
+                // i - 1, j     -> top middle
+                partSum += findAdjacency(inputGrid, i - 1, j);
+
+                // i - 1, j + 1 -> top right
+                partSum += findAdjacency(inputGrid, i - 1, j + 1);
+
+                // i    , j - 1 -> middle left
+                partSum += findAdjacency(inputGrid, i, j - 1);
+
+                // i    , j + 1 -> middle right
+                partSum += findAdjacency(inputGrid, i, j + 1);
+
+                // i + 1, j - 1 -> bottom left
+                partSum += findAdjacency(inputGrid, i + 1, j - 1);
+
+                // i + 1, j     -> bottom middle
+                partSum += findAdjacency(inputGrid, i + 1, j);
+
+                // i + 1, j + 1 -> bottom right
+                partSum += findAdjacency(inputGrid, i + 1, j + 1);
+            }
+        }
+    }
+    std::cout << "Total Sum: " << partSum << std::endl;
 }
