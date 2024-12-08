d85ce0ce26941b3530a76295d095f0ae7a622140
James Andrews <cjand225@gmail.com>
2024-12-08 13:39:29 -0700

    chore: clang format

--- 2024/problems/day-1/partOne.cpp
+++ 2024/problems/day-1/partOne.cpp
@@ -1,44 +1,44 @@
+#include <stdlib.h>
+
 #include <algorithm>
 #include <fstream>
 #include <iostream>
 #include <sstream>
-#include <stdlib.h>
 #include <string>
 #include <vector>
 
 int main() {
+    // Read in the file
+    std::fstream inputFile("input.txt", std::fstream::in);
+    std::string inputString = "";
+    std::stringstream ss;
 
-  // Read in the file
-  std::fstream inputFile("input.txt", std::fstream::in);
-  std::string inputString = "";
-  std::stringstream ss;
+    // Split the line input, left to left list, right to right list
+    std::vector<int> leftElements;
+    std::vector<int> rightElements;
+    int left, right;
+    while (inputFile >> left >> right) {
+        leftElements.push_back(left);
+        rightElements.push_back(right);
+    }
 
-  // Split the line input, left to left list, right to right list
-  std::vector<int> leftElements;
-  std::vector<int> rightElements;
-  int left, right;
-  while (inputFile >> left >> right) {
-    leftElements.push_back(left);
-    rightElements.push_back(right);
-  }
+    std::sort(leftElements.begin(), leftElements.end());
+    std::sort(rightElements.begin(), rightElements.end());
+    // sort both lists
+    // iterate over all lists
+    int distance = 0;
+    int size = 0;
 
-  std::sort(leftElements.begin(), leftElements.end());
-  std::sort(rightElements.begin(), rightElements.end());
-  // sort both lists
-  // iterate over all lists
-  int distance = 0;
-  int size = 0;
+    // subtract each iteration and add it to the distance.
+    if (leftElements.size() > rightElements.size()) {
+        size = leftElements.size();
+    } else {
+        size = rightElements.size();
+    }
 
-  // subtract each iteration and add it to the distance.
-  if (leftElements.size() > rightElements.size()) {
-    size = leftElements.size();
-  } else {
-    size = rightElements.size();
-  }
+    for (int i = 0; i < size; i++) {
+        distance += abs(leftElements[i] - rightElements[i]);
+    }
 
-  for (int i = 0; i < size; i++) {
-    distance += abs(leftElements[i] - rightElements[i]);
-  }
-
-  std::cout << distance << std::endl;
+    std::cout << distance << std::endl;
 }
--- 2024/problems/day-1/partTwo.cpp
+++ 2024/problems/day-1/partTwo.cpp
@@ -1,48 +1,48 @@
+#include <stdlib.h>
+
 #include <algorithm>
 #include <fstream>
 #include <iostream>
 #include <map>
 #include <sstream>
-#include <stdlib.h>
 #include <string>
 #include <vector>
 
 int main() {
+    // Read in the file
+    std::fstream inputFile("input.txt", std::fstream::in);
+    std::string inputString = "";
+    std::stringstream ss;
 
-  // Read in the file
-  std::fstream inputFile("input.txt", std::fstream::in);
-  std::string inputString = "";
-  std::stringstream ss;
-
-  // Split the line input, left to left list, right to right list
-  std::vector<int> leftElements;
-  std::vector<int> rightElements;
-  std::map<int, int> occurences;
-  int left, right;
-  while (inputFile >> left >> right) {
-    if (occurences.find(left) == occurences.end()) {
-      occurences[left] = 0;
+    // Split the line input, left to left list, right to right list
+    std::vector<int> leftElements;
+    std::vector<int> rightElements;
+    std::map<int, int> occurences;
+    int left, right;
+    while (inputFile >> left >> right) {
+        if (occurences.find(left) == occurences.end()) {
+            occurences[left] = 0;
+        }
+        leftElements.push_back(left);
+        rightElements.push_back(right);
     }
-    leftElements.push_back(left);
-    rightElements.push_back(right);
-  }
 
-  // sort both lists
-  std::sort(leftElements.begin(), leftElements.end());
-  std::sort(rightElements.begin(), rightElements.end());
+    // sort both lists
+    std::sort(leftElements.begin(), leftElements.end());
+    std::sort(rightElements.begin(), rightElements.end());
 
-  int similarity = 0;
-  int size = leftElements.size();
+    int similarity = 0;
+    int size = leftElements.size();
 
-  // find out occurences in right list
-  for (int i = 0; i < size; i++) {
-    occurences[rightElements[i]] += 1;
-  }
+    // find out occurences in right list
+    for (int i = 0; i < size; i++) {
+        occurences[rightElements[i]] += 1;
+    }
 
-  // iterate and find similarity score
-  for (int i = 0; i < size; i++) {
-    similarity += leftElements[i] * occurences[leftElements[i]];
-  }
+    // iterate and find similarity score
+    for (int i = 0; i < size; i++) {
+        similarity += leftElements[i] * occurences[leftElements[i]];
+    }
 
-  std::cout << similarity << std::endl;
+    std::cout << similarity << std::endl;
 }
